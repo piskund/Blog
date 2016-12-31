8993ec08932a71a2ede542ef7517bc4ec757856c
dip <dmytro.piskun@gmail.com>
2016-12-31 12:11:06 +0200

    Add project files.

---
+++ Crossover/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Crossover.Startup))]
+namespace Crossover
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
